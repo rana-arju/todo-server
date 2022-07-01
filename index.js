538d3433ee30cbbec8f1527bc49a00c382d55c51
Rana Arju <arjurana530@gmail.com>
2022-07-01 12:05:44 +0600

    list delete functionality add

--- index.js
+++ index.js
@@ -18,42 +18,41 @@ async function run() {
     const todoAddCollections = client.db("todoCollection").collection("addlist");
     const todoComplateCollections = client.db("todoCollection").collection("complateList");
 
-
-
     //todo list post
     app.post("/todo", async(req, res) => {
         const list = req.body;
          const result = await todoAddCollections.insertOne(list);
         res.send(result);
     })
+
    //todo list Get
    app.get('/todo', async(req, res) => {
       const lists = await todoAddCollections.find().toArray();
       res.send(lists);
    });
-   //todo one list update
-   app.put('/todo/:id', async(req, res) => {
-      const lists = await todoAddCollections.find({}).toArray();
-      res.send(lists);
+  app.delete('/todo/:id', async(req, res) => {
+    const id = req.params.id;
+     const query = {_id: ObjectId(id)};
+     const result = await todoAddCollections.deleteOne(query);
+      res.send(result);
    });
    //todo list complate
-  //  app.put('/todo/complate/:id', async(req, res) => {
-  //     const id = req.params.id;
-  //     const query = {_id: ObjectId(id).trim()};
-  //     const options = {upsert: true};
-  //     const updateDoc = {
-  //       $set:{
-  //         complate: true,
-  //       }
-  //     };
-  //     const result = await todoAddCollections.updateOne(query, updateDoc, options);
-  //     // res.send(result);
-  //  });
-   //todo list Get
-   app.delete('/todo/:id', async(req, res) => {
+   app.get('/todo/complate', async(req, res) => {
+     const result = await todoComplateCollections.find({}).toArray();
+      res.send(result);
+   });
+
+   //post on "todoComplateCollections" when complete button click
+   app.post('/todo/complate', async(req, res) => {
+     const query = req.body;
+     const result = await todoComplateCollections.insertOne(query);
+      res.send(result);
+   });
+   //delete list from "todoAddCollections" when task complete
+   app.delete('/todo/complate/:id', async(req, res) => {
     const id = req.params.id;
-    const query = {_id: ObjectId(id)}
-    const result = await todoAddCollections.deleteOne(query);
+     const query = {todo: id};
+     const result = await todoAddCollections.deleteOne(query);
       res.send(result);
    });
 
